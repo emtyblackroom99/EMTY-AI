68110f528ee51acb77d0ef73c5194d61a42ff936
emtyblackroom99 <emtyebr@gmail.com>
2025-06-11 09:52:06 -0700

    src file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+﻿function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1 style={{ color: 'green' }}>
+        ✅ EMTY Projesi Başarıyla Çalışıyor!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
